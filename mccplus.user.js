e36db60c1e2796fee3bf38d065832dcf73a7892b
Miikka Virtanen <dragory.up@gmail.com>
2015-05-20 23:03:13 +0300

    Add an auto-updating userscript version.

---
+++ mccplus.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name       MCC+
+// @version    0.1
+// @match      http://trugul.com/
+// @match      http://trugul.com/index.php
+// ==/UserScript==
+
+function loadScript() {
+	var script = document.createElement('script');
+	script.src = 'https://cdn.rawgit.com/phenomist/mccplus/master/mccplus.js?t=' + (new Date()).getTime().toString();
+	document.body.appendChild(script);
+}
+
+var hotReloadButton = document.createElement('a');
+hotReloadButton.innerText = 'Reload MCC+';
+hotReloadButton.href = 'javascript:void(0)';
+hotReloadButton.addEventListener('click', loadScript);
+
+var hotReloadButtonContainer = document.createElement('li');
+hotReloadButtonContainer.appendChild(hotReloadButton);
+
+document.querySelector('#navbar .nav').appendChild(hotReloadButtonContainer);
+
+loadScript();
