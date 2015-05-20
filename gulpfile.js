7fc225af77788c09c0d63c568d276fb3de77678c
Miikka Virtanen <dragory.up@gmail.com>
2015-05-20 23:02:27 +0300

    Fix gulpfile + specify some jsbeautifier config values

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,12 @@ var gulp = require('gulp'),
     prettify = require('gulp-jsbeautifier'),
     jshint = require('gulp-jshint');
 
-gulp.task('default', [
-	gulp.src('mccplus.js')
+gulp.task('default', function() {
+	return gulp.src('mccplus.js')
 	    .pipe(jshint())
 		.pipe(prettify({
 			config: '.jsbeautifierrc',
 			mode: 'VERIFY_AND_WRITE'
 		}))
-]);
+		.pipe(gulp.dest('.'));
+});
